973318ade84d837f1b5fce9f55c1d318418deb5b
Yuji Chinen <veloxml1113@gmail.com>
2025-02-21 22:48:47 +0900

    macOS用のC/C++コンパイラのデフォルト設定をアーキテクチャに応じて変更し、環境変数が未設定の場合にデフォルト値を使用するように修正

--- setup.py
+++ setup.py
@@ -17,8 +17,24 @@ import subprocess
 if sys.platform == "darwin":  # macOS
     TBB_PREFIX = os.popen("brew --prefix tbb").read().strip()
     TBB_DIR = os.path.join(TBB_PREFIX, "lib/cmake/tbb")
-    CC = os.environ.get("CMAKE_C_COMPILER", "/opt/homebrew/bin/gcc-13")
-    CXX = os.environ.get("CMAKE_CXX_COMPILER", "/opt/homebrew/bin/g++-13")
+    
+    # 実行中のマシンのアーキテクチャを判別 (arm64: Apple Silicon, x86_64: Intel)
+    machine = sys.platform.machine()
+    if machine == "arm64":
+        default_cc = "/opt/homebrew/bin/gcc-13"
+        default_cxx = "/opt/homebrew/bin/g++-13"
+    else:
+        default_cc = "/usr/local/opt/gcc/bin/gcc-13"
+        default_cxx = " /usr/local/opt/gcc/bin/g++-13"
+    
+    # 環境変数に値が設定されていなければデフォルト値を利用
+    CC = os.environ.get("CMAKE_C_COMPILER", default_cc)
+    CXX = os.environ.get("CMAKE_CXX_COMPILER", default_cxx)
+
+    # 確認用の出力
+    print("TBB_DIR:", TBB_DIR)
+    print("C Compiler:", CC)
+    print("C++ Compiler:", CXX)
 
 elif sys.platform.startswith("linux"):
     TBB_DIR = os.environ.get("TBB_DIR", "/usr/lib/x86_64-linux-gnu/cmake/TBB")
